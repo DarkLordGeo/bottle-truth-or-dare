2f0954d28bd5dab21c9589e6bef3cc64a0459f2f
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2024-11-15 23:22:20 +0400

    test

--- src/script.js
+++ src/script.js
@@ -5,16 +5,38 @@ const bottle = document.querySelector("#bottle")
 const bottleImage = document.querySelectorAll("#bottleImage")
 const bottleGrid = document.querySelector(".bottle-grid")
 
+const spinAudio = document.querySelector("#spinAudio")
 
 function openMenu(){
     menu.classList.toggle("flex")
 }
-
 function openSubMenu(){
     bottleGrid.classList.toggle("subMenuBg")
 }
-
 function changeContent(img){
-    img.style.width = "300px"
     bottle.src = img.src
+    bottle.style.width = "calc(78vh - 350px)"
+}
+let animStart = false
+let keyframes = `
+     @keyframes spin {
+    0%{
+        transform:rotate(0deg);
+    }
+    50%{
+        transform:rotate(${Math.random()*180});
+        
+    }
+    100%{
+        transform:rotate(${Math.random()*360});
+    }
+} 
+`
+function action(){
+    if(spinAudio.play()){
+        animStart = true
+    }
+    if(animStart == true){
+        bottleImage.style = keyframes
+    }
 }
